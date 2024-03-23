b5a89f70b78cdf1cf97ea35144c19d5000dc8e76
adrianap0607 <132533905+adrianap0607@users.noreply.github.com>
2024-03-23 16:15:48 -0600

    Documentación AtomNode.java

--- demo/src/main/java/AtomNode.java
+++ demo/src/main/java/AtomNode.java
@@ -1,22 +1,32 @@
 import java.util.List;
 
+// Implementación de la interfaz Node para representar un nodo átomo en una estructura de árbol
 public class AtomNode implements Node {
+    // Valor almacenado en el nodo átomo
     private Object value;
 
+    // Constructor que recibe el valor del átomo
     public AtomNode(Object value) {
         this.value = value;
     }
 
+    // Método para obtener el valor del átomo
+    // @return El valor del átomo
     public Object getValue() {
         return value;
     }
 
+    // Método para evaluar el nodo átomo utilizando un evaluador específico
+    // @param evaluator El evaluador a utilizar para la evaluación
+    // @return El valor del átomo
     @Override
     public Object evaluate(Evaluator evaluator) {
         // Devolver el valor del átomo
         return value;
     }
 
+    // Método para representar el nodo átomo como una cadena de texto
+    // @return Representación en cadena de texto del valor del átomo
     @Override
     public String toString() {
         return value.toString();
