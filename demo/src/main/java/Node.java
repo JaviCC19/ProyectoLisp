edab3e075d678066102f96f80acf3f5912148ab6
adrianap0607 <132533905+adrianap0607@users.noreply.github.com>
2024-03-23 16:18:54 -0600

    Documentación Node.java

--- demo/src/main/java/Node.java
+++ demo/src/main/java/Node.java
@@ -1,4 +1,5 @@
+// Interfaz que define un nodo en la estructura del árbol de análisis sintáctico
 public interface Node {
-    // Método para evaluar el nodo en el contexto del evaluador
+    // Método para evaluar el nodo en el contexto del evaluador y devolver un resultado
     Object evaluate(Evaluator evaluator);
 }
